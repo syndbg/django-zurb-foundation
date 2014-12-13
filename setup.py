80357c2a747e8a5e2ba7605e13b0154e7894b93c
syndbg <syndbe@gmail.com>
2014-12-14 00:43:45 +0200

    Moved version to foundation module

--- setup.py
+++ setup.py
@@ -2,18 +2,27 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 import os
+import os.path
+
+
+def _get_version(here):
+    with open(os.path.join(here, "foundation", "__init__.py")) as f:
+        for line in f.readlines():
+            if line.startswith("__version__ ="):
+                return line.split("=")[1].strip().strip('"')
 
 ROOT_DIR = os.path.dirname(__file__)
 SOURCE_DIR = os.path.join(ROOT_DIR)
+VERSION = _get_version(ROOT_DIR)
 
 setup(
     name="django-zurb-foundation",
-    version="5.4.7.1",
+    version=VERSION,
     description="Django Zurb Foundation package.",
     author="Amar Šahinović",
     author_email="amar@sahinovic.com",
     url="https://github.com/amarsahinovic/django-zurb-foundation",
-    license='BSD License', 
+    license='BSD License',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
@@ -27,7 +36,5 @@ setup(
                    'Programming Language :: Python :: 2',
                    'Programming Language :: Python :: 3',
                    'Topic :: Utilities'],
-    install_requires=[
-
-    ],
+    install_requires=[],
 )
